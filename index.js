10684db9ac0bfa2aacfac42b51540e8aca6bc7b1
niveKKumar <mail.kumarkevin@gmail.com>
2021-12-07 10:11:30 +0100

    publishtest

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-var http = require('http');
+var http = require("http");
 
-var server = http.createServer(function (request, response){
-  response.writeHead(200, {"Content-Type": "text/plain"});
+var server = http.createServer(function (request, response) {
+  response.writeHead(200, { "Content-Type": "text/plain" });
   response.end("Hello World from expressjs in a Docker container.");
 });
 
